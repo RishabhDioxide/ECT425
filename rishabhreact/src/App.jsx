07ca435e141b87f6bf8f08536485feb44afcc7a1
Vistup <peroxydisulfate@gmail.com>
2025-02-05 11:02:36 +0530

    Task 3

---
+++ rishabhreact/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+                const data = await response.json();
+                console.log(data);
+                setUsers(data);
+            } catch (e) {
+                console.error("Error fetching user data:", e);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Rishabh Raj</h1>
+            <h1>Roll Number - 1/22/FET/BCS/425</h1>
+
+        </div>
+    );
+}
+
+export default App;
